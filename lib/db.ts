36fc86c1d37dd0b0460e8dbe443ddf55aa0987fd
Anurag Pathak <anuragpathak911@gmail.com>
2025-03-05 22:19:33 +0530

    Add mongoose connection handling and integrate daisyUI with Tailwind CSS

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+let cache = global.mongoose
+if (!cache) {
+	cache = global.mongoose = { conn: null, promise: null }
+}
+
+export async function connectToDb() {
+	if (cache.conn) {
+		return cache.conn
+	}
+
+	if (!cache.promise) {
+		cache.promise = mongoose.connect(process.env.MONGODB_URI!, {
+			maxPoolSize: 10,
+		})
+		.then(() => mongoose.connection)
+	}
+
+	cache.conn = await cache.promise
+	return cache.conn
+}
